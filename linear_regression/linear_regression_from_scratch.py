9fbaba7f4e62be5c645663502b87c4ccb269486f
CryAndRRich <willshane2006@gmail.com>
2025-01-06 18:38:05 +0700

    Update linear_regression_from_scratch.py

--- linear_regression/linear_regression_from_scratch.py
+++ linear_regression/linear_regression_from_scratch.py
@@ -1,39 +1,37 @@
-def cost_function(x_train, y_train, m, b, batch_size):
+def cost_function(x_train, y_train, weight, bias, batch_size):
     total_cost = 0
 
     for i in range(batch_size):
         x = x_train[i]
         y = y_train[i]
-        total_cost += (y - (m * x + b)) ** 2
+        total_cost += (y - (weight * x + bias)) ** 2
     
     avg_cost = total_cost / batch_size
     return avg_cost
 
-def step_gradient(x_train, y_train, m, b, learn_rate, batch_size):
-    m_gradient = 0
-    b_gradient = 0
+def gradient_descent(x_train, y_train, weight, bias, learn_rate, batch_size):
+    weight_gradient = 0
+    bias_gradient = 0
 
     for i in range(batch_size):
         x = x_train[i]
         y = y_train[i]
 
-        m_gradient += -(2 / batch_size) * x * (y - ((m * x) + b))
-        b_gradient += -(2 / batch_size) * (y - ((m * x) + b))
+        weight_gradient += -(2 / batch_size) * x * (y - ((weight * x) + bias))
+        bias_gradient += -(2 / batch_size) * (y - ((weight * x) + bias))
     
-    m -= (learn_rate * m_gradient)
-    b -= (learn_rate * b_gradient)
+    weight -= (learn_rate * weight_gradient)
+    bias -= (learn_rate * bias_gradient)
 
-    return m, b
+    return weight, bias
 
-def train_model_scratch(x_train, y_train, learn_rate, number_of_epochs, batch_size):
-    m, b = 0, 0
+def train_model_scratch(x_train, y_train, learn_rate, batch_size, number_of_epochs):
+    weight, bias = 0, 0
     for epoch in range(number_of_epochs + 1):
-        m, b = step_gradient(x_train, y_train, m, b, learn_rate, batch_size)
+        weight, bias = gradient_descent(x_train, y_train, weight, bias, learn_rate, batch_size)
 
         if epoch % 100 == 0:
-            cost = cost_function(x_train, y_train, m, b, batch_size)
-            print('Epoch: {:4d}/{} m: {:.3f}, b: {:.3f} Cost: {:.6f}'.format(
-                  epoch, number_of_epochs, m, b, cost))
-
-
-
+            cost = cost_function(x_train, y_train, weight, bias, batch_size)
+            print('Epoch: {:4d}/{} Weight: {:.3f}, Bias: {:.3f} Cost: {:.6f}'.format(
+                  epoch, number_of_epochs, weight, bias, cost))
+            
