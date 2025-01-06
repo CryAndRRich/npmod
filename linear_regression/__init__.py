c77414cc8340e188a77fdc4be2dc8a514205df78
CryAndRRich <willshane2006@gmail.com>
2025-01-06 18:35:48 +0700

    Update __init__.py

--- linear_regression/__init__.py
+++ linear_regression/__init__.py
@@ -1,2 +1,3 @@
+from .linear_regression_data import LinearRegressionData
 from .linear_regression_from_scratch import train_model_scratch
 from .linear_regression_with_pytorch import train_model_pytorch
