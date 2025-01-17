e0148ad40c039058af2d18b55851dae888ec36ef
CryAndRRich <willshane2006@gmail.com>
2025-01-17 10:46:30 +0700

    Update class

--- support_vector_machines/svm.py
+++ support_vector_machines/svm.py
@@ -1,29 +1,30 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from base_model import ModelML
 
-torch.manual_seed(777)
+torch.manual_seed(42)
 
-class SVMModel(nn.Module):
-    def __init__(self, x_train, kernel='rbf', gamma=1.0, train_gamma=True, degree=4, b=1.0):
+class SVMModule(nn.Module):
+    def __init__(self, features, kernel="rbf", gamma=0.1, train_gamma=True, degree=3, b=1.0):
         super().__init__()
-        self.train_data = x_train
+        self.train_data = features
         self.gamma = torch.nn.Parameter(torch.FloatTensor([gamma]), requires_grad=train_gamma)
         self.b = b
 
-        if kernel == 'linear':
+        if kernel == "linear":
             self.kernel = self.linear
-            self.C = x_train.size(1)
-        elif kernel == 'rbf':
+            self.C = features.size(1)
+        elif kernel == "rbf":
             self.kernel = self.rbf
-            self.C = x_train.size(0)
-        elif kernel == 'poly':
+            self.C = features.size(0)
+        elif kernel == "poly":
             self.kernel = self.poly
-            self.C = x_train.size(0)
+            self.C = features.size(0)
             self.degree = degree
-        elif kernel == 'sigmoid':
+        elif kernel == "sigmoid":
             self.kernel = self.sigmoid
-            self.C = x_train.size(0)
+            self.C = features.size(0)
         else: 
             raise ValueError(f"Unsupported kernel '{kernel}'. Supported kernels are 'linear', 'rbf', 'poly', and 'sigmoid'")
 
@@ -52,47 +53,36 @@ class SVMModel(nn.Module):
         y = self.weight(y)
         return y
 
-    
-def train_model(x_train, y_train, x_test, y_test, learn_rate, number_of_epochs):
-    model_linear = SVMModel(x_train, kernel='linear')
-    optimizer_linear = optim.SGD(model_linear.parameters(), lr=learn_rate)
-    
-    model_rbf= SVMModel(x_train, kernel='rbf')
-    optimizer_rbf = optim.SGD(model_rbf.parameters(), lr=learn_rate)
-    
-    model_poly = SVMModel(x_train, kernel='poly')
-    optimizer_poly = optim.SGD(model_poly.parameters(), lr=learn_rate)
-    
-    model_sigmoid= SVMModel(x_train, kernel='sigmoid')
-    optimizer_sigmoid = optim.SGD(model_sigmoid.parameters(), lr=learn_rate)
+class SVMModel(ModelML):
+    def __init__(self, learn_rate, number_of_epochs, kernel="linear", gamma=0.1):
+        self.learn_rate = learn_rate
+        self.number_of_epochs = number_of_epochs
+        self.kernel = kernel
+        self.gamma = gamma
 
-    for epoch in range(number_of_epochs + 1):
-        linear_prediction = model_linear(x_train)
-        rbf_prediction = model_rbf(x_train)
-        poly_prediction = model_poly(x_train)
-        sigmoid_prediction = model_sigmoid(x_train)
-        
-        linear_cost = model_linear.hinge_loss(linear_prediction, y_train.unsqueeze(1))
-        rbf_cost = model_rbf.hinge_loss(rbf_prediction, y_train.unsqueeze(1))
-        poly_cost = model_poly.hinge_loss(poly_prediction, y_train.unsqueeze(1))
-        sigmoid_cost = model_sigmoid.hinge_loss(sigmoid_prediction, y_train.unsqueeze(1))
-        
-        optimizer_linear.zero_grad()
-        optimizer_rbf.zero_grad()
-        optimizer_poly.zero_grad()
-        optimizer_sigmoid.zero_grad()
+    def fit(self, features, labels):
+        self.model = SVMModule(features, self.kernel, self.gamma)
+        optimizer = optim.SGD(self.model.parameters(), lr=self.learn_rate)
 
-        linear_cost.backward()
-        rbf_cost.backward()
-        poly_cost.backward()
-        sigmoid_cost.backward()
+        self.model.train()
+        for _ in range(self.number_of_epochs):
+            predictions = self.model(features)
+            cost = self.model.hinge_loss(predictions, labels.unsqueeze(1))
 
-        optimizer_linear.step()
-        optimizer_rbf.step()
-        optimizer_poly.step()
-        optimizer_sigmoid.step()
-        
-        if epoch % 100 == 0:
-            print('Epoch: {:4d}/{} Linear Cost: {:.4f} RBF Cost: {:.4f} Polynomial Cost: {:.4f} Sigmoid Cost: {:.4f}'.format(
-                  epoch, number_of_epochs, linear_cost.item(), rbf_cost.item(), poly_cost.item(), sigmoid_cost.item()))
+            optimizer.zero_grad()
+            cost.backward()
+            optimizer.step()
+        self.model.eval()
+    
+    def predict(self, test_features, test_labels):
+        with torch.no_grad():
+            predictions = (self.model(test_features)).detach().numpy()
+            predictions = (predictions > predictions.mean())
+            test_labels = test_labels.detach().numpy()
             
+            accuracy, f1 = self.evaluate(predictions, test_labels)
+            print("Epoch: {}/{} Accuracy: {:.5f} F1-score: {:.5f}".format(
+                   self.number_of_epochs, self.number_of_epochs, accuracy, f1))
+    
+    def __str__(self):
+        return "Support Vector Machine (SVM) - Kernel:'{}'".format(self.kernel)
