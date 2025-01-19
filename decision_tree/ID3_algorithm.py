66d7cdeb000414b924243e8e7e2ff4c487b6321c
CryAndRRich <willshane2006@gmail.com>
2025-01-19 15:43:23 +0700

    Add id3 tree

---
+++ decision_tree/ID3_algorithm.py
@@ -0,0 +1,59 @@
+import numpy as np
+from base_model import ModelML
+from .tree import TreeNode
+from .utils import *
+
+class ID3DecisionTree(ModelML):
+    def fit(self, features, labels):
+        self.decision_tree = self.build_tree(features, labels)
+    
+    def build_tree(self, features, labels):
+        best_gain = 0
+        best_criteria = None
+        best_sets = None
+        _, n = features.shape
+
+        current_entropy = entropy(labels)
+
+        for feature in range(n):
+            feature_values = set(features[:, feature])
+            for value in feature_values:
+                true_features, true_label, false_feature, false_label = split_data(features, labels, feature, value)
+                true_entropy = entropy(true_label)
+                false_entropy = entropy(false_label)
+                p = len(true_label) / len(labels)
+                information_gain = current_entropy - p * true_entropy - (1 - p) * false_entropy
+
+                if information_gain > best_gain:
+                    best_gain = information_gain
+                    best_criteria = (feature, value)
+                    best_sets = (true_features, true_label, false_feature, false_label)
+
+        if best_gain > 0:
+            true_branch = self.build_tree(best_sets[0], best_sets[1])
+            false_branch = self.build_tree(best_sets[2], best_sets[3])
+            return TreeNode(feature=best_criteria[0], value=best_criteria[1], true_branch=true_branch, false_branch=false_branch)
+
+        return TreeNode(results=labels[0])
+
+    def predict(self, test_features, test_labels):
+        num_samples, _ = test_features.shape
+
+        predictions = np.zeros(num_samples)
+        for ind, feature in enumerate(test_features):
+            predictions[ind] = self.predict_node(self.decision_tree, feature)
+
+        accuracy, f1 = self.evaluate(predictions, test_labels)
+        print("Accuracy: {:.5f} F1-score: {:.5f}".format(accuracy, f1))
+    
+    def predict_node(self, tree, sample):
+        if tree.results is not None:
+            return tree.results
+        else:
+            branch = tree.false_branch
+            if sample[tree.feature] <= tree.value:
+                branch = tree.true_branch
+            return self.predict_node(branch, sample)
+    
+    def __str__(self):
+        return "Decision Trees: ID3 Algorithm"
