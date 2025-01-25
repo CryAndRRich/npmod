f006b9fa5669dc96ce1175d745b41c978044bb95
CryAndRRich <willshane2006@gmail.com>
2025-01-25 17:24:51 +0700

    __init__ file

---
+++ npmod/nn/__init__.py
@@ -0,0 +1,5 @@
+from .activations import ReLU, LeakyReLU, Sigmoid, Tanh
+from .layers import Linear, Dropout, Flatten
+from .container import Sequential
+from .losses import MAE, MSE, MALE, RSquared, MAPE, wMAPE, SmoothL1, CE, BCE, KLDiv
+from .optimizers import GD, SGD, AdaGrad, RMSprop, Adam
