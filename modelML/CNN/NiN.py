d5f0774f23def97b4c13d28c91ef4a0ffe29658c
CryAndRRich <willshane2006@gmail.com>
2025-02-06 18:44:09 +0700

    Add vgg and nin

---
+++ modelML/CNN/NiN.py
@@ -0,0 +1,94 @@
+import torch.nn as nn
+import torch.optim as optim
+from ..CNN import Reshape, ConvNet
+
+def nin_block(in_channels: int,
+              out_channels: int,
+              kernel_size: int,
+              stride: int,
+              padding: int) -> nn.Sequential:
+    """
+    Creates a NiN block consisting of:
+      - A main convolution with the given kernel size, stride, and padding,
+      - Followed by two successive 1x1 convolutions for nonlinear feature extraction
+    
+    Each convolution is followed by a ReLU activation
+    
+    --------------------------------------------------
+    Parameters:
+        in_channels: Number of input channels
+        out_channels: Number of output channels
+        kernel_size: Kernel size for the main convolution
+        stride: Stride for the main convolution
+        padding: Padding for the main convolution
+    
+    --------------------------------------------------
+    Returns:
+        nn.Sequential: The constructed NiN block.
+    """
+    block = nn.Sequential(
+        nn.Conv2d(in_channels=in_channels,
+                  out_channels=out_channels,
+                  kernel_size=kernel_size,
+                  stride=stride,
+                  padding=padding),
+        nn.BatchNorm2d(num_features=out_channels),
+        nn.ReLU(inplace=True),
+        
+        nn.Conv2d(in_channels=out_channels,
+                  out_channels=out_channels,
+                  kernel_size=1),
+        nn.BatchNorm2d(num_features=out_channels),
+        nn.ReLU(inplace=True),
+        
+        nn.Conv2d(in_channels=out_channels,
+                  out_channels=out_channels,
+                  kernel_size=1),
+        nn.BatchNorm2d(num_features=out_channels),
+        nn.ReLU(inplace=True)
+    )
+    
+    return block
+
+class NiN(ConvNet):
+    """
+    NiN (Network in Network) implementation adapted for 28x28 input images
+    
+    The architecture is composed of multiple NiN blocks, interleaved with pooling layers,
+    dropout, and ends with global pooling and flattening before classification
+    """
+    def init_network(self):
+        # Adjust the blocks for 28x28 input images:
+        # Block1: Use a smaller kernel (5x5) with stride=1 and padding=2 to preserve spatial dimensions.
+        block1 = nin_block(in_channels=1, out_channels=96, kernel_size=5, stride=1, padding=2)
+        # Block2: A standard NiN block with 3x3 convolution.
+        block2 = nin_block(in_channels=96, out_channels=256, kernel_size=3, stride=1, padding=1)
+        # Block3: Similar configuration as block2.
+        block3 = nin_block(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1)
+        # Block4: Final block for classification with 10 filters.
+        block4 = nin_block(in_channels=384, out_channels=10, kernel_size=3, stride=1, padding=1)
+        
+        self.network = nn.Sequential(
+            Reshape(),
+            
+            block1,
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            block2,
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            block3,
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=0.5),
+            
+            block4,
+            nn.AdaptiveAvgPool2d(output_size=(1, 1)),
+            nn.Flatten()
+        )
+        
+        self.network.apply(self.init_weights)
+        self.optimizer = optim.SGD(self.network.parameters(), lr=self.learn_rate)
+        self.criterion = nn.CrossEntropyLoss()
+
+    def __str__(self):
+        return "Convolutional Neural Networks: NiN"
---
+++ modelML/CNN/VGG.py
@@ -0,0 +1,82 @@
+import torch.nn as nn
+import torch.optim as optim
+from ..CNN import Reshape, ConvNet
+    
+def vgg_block(num_convs: int, 
+              in_channels: int, 
+              out_channels: int,
+              use_adaptive_pool: bool = False) -> nn.Sequential:
+    """
+    Implementation of one VGG block consists of a sequence of convolutional layers, 
+    followed by a max pooling layer for spatial downsampling
+
+    --------------------------------------------------
+    Parameters:
+        num_convs: Number of convolutional layers
+        in_channels: Number of input channels
+        out_channels: Number of output channels
+        use_adaptive_pool: If True, the pooling layer is replaced with AdaptiveMaxPool2d
+    
+    --------------------------------------------------
+    Returns:
+        block: One VGG block
+    """
+    layers = []
+    for _ in range(num_convs):
+        layers.append(nn.Conv2d(in_channels=in_channels, 
+                                out_channels=out_channels, 
+                                kernel_size=3, 
+                                padding=1))
+        layers.append(nn.BatchNorm2d(num_features=out_channels))
+        layers.append(nn.ReLU(inplace=True))
+        in_channels = out_channels
+    
+    if use_adaptive_pool:
+        layers.append(nn.AdaptiveMaxPool2d((1, 1)))
+    else:
+        layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+    
+    block = nn.Sequential(*layers)
+    return block
+
+class VGG(ConvNet):
+    def init_network(self):
+        # A list of tuples (one per block), where each contains two values: 
+        # the number of convolutional layers and the number of output channels
+        conv_arch = ((1, 64), (1, 128), (2, 256), (2, 512), (2, 512))
+        
+        conv_layers = []
+        in_channels = 1
+        
+        for i, (num_convs, out_channels) in enumerate(conv_arch):
+            if i == len(conv_arch) - 1:
+                block = vgg_block(num_convs, in_channels, out_channels, use_adaptive_pool=True)
+            else:
+                block = vgg_block(num_convs, in_channels, out_channels)
+            conv_layers.append(block)
+            in_channels = out_channels
+        
+        self.network = nn.Sequential(
+            Reshape(),
+
+            *conv_layers,
+
+            nn.Flatten(),
+            nn.Linear(in_features=512, out_features=4096),
+            nn.BatchNorm1d(num_features=4096),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.BatchNorm1d(num_features=4096),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=4096, out_features=10)
+        )
+        self.network.apply(self.init_weights)
+        self.optimizer = optim.SGD(self.network.parameters(), lr=self.learn_rate)
+        self.criterion = nn.CrossEntropyLoss()
+    
+    def __str__(self):
+        return "Convolutional Neural Networks: VGG-11"
